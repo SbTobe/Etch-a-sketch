699a02a2e5401644ac7bdb591ffd6d45dfb06ecb
Toby <sbtobe85@gmail.com>
2022-09-03 13:02:02 +0100

    added color pick functionality and clear

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
+let color = document.querySelector(".clr");
+
 function populateBoard(size) {
-let board = document.querySelector(".board");
-let squares = board.querySelectorAll("div")
-squares.forEach((div) => div.remove())
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-let amount = size * size
+  let amount = size * size;
 
-    for (let i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.addEventListener("mouseover", ()=> square.style.backgroundColor="purple")
+    square.addEventListener("mouseover", colorSquare);
     square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
-    }
+  }
 }
 
 populateBoard(16);
 
-function changeSize(input){
-    if (input >=2 && input <=100){
-        populateBoard(input);
-    } else {
-        console.log("to high or too low");
-    }
-    
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("to high or too low");
+  }
+}
+
+function colorSquare() {
+  this.style.backgroundColor = color.value;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+function changeColor(choice) {
+  color = choice;
 }
